bf6933ed73696df598960f06b8815ac88b87d24d
emaemae <116826949+emaemae@users.noreply.github.com>
2022-11-11 12:59:38 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector('#nav');
+const navBtn = document.querySelector('#nav-btn');
+const navBtnImg = document.querySelector('#nav-btn-img');
+
+navBtn.ontouchend = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "img/close.png";
+    } else {
+        navBtnImg.src = 'img/burger.png';
+    }
+}
+navBtn.onclick = () => {
+    if (nav.classList.toggle('open')) {
+        navBtnImg.src = "img/close.png";
+    } else {
+        navBtnImg.src = 'img/burger.png';
+    }
+}
+
+AOS.init();
